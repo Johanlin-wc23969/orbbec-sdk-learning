a3639e55b0369f2a2af19aba37e95844ee781738
Johanlin-wc23969 <wc23969@bristol.ac.uk>
2026-06-05 14:26:48 +0100

    Week1-Day5: Perfect Forwarding

--- weeks/week1/day4/move_semantic.cpp
+++ weeks/week1/day4/move_semantic.cpp
@@ -15,6 +15,37 @@ void example1(){
     func(10);
 
 }
+
+class Buffer{
+public:
+    size_t size;
+    int* data;
+
+    // 构造函数
+    Buffer(size_t size):size(size){
+        data = new int[size];
+    }
+
+    // 析构函数
+    ~Buffer(){
+        delete[] data;
+    }
+
+    // 拷贝构造
+    Buffer(const Buffer& other){
+        size = other.size;
+        data = new int[size];
+        std::copy(other.data, other.data+size, data);
+    }
+
+    // 移动构造
+    Buffer(Buffer&& other){
+        size = other.size;
+        data = other.data;
+        other.size = 0;
+        other.data = nullptr;
+    }
+};
 int main() {
     example1();
 
